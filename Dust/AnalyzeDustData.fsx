ee995ff365080e5cf38f5a0a836ba8596960587d
Alexandre Radicchi <alex.radi@gmail.com>
2017-03-04 16:07:30 +0100

    Started Analysis

--- Dust/AnalyzeDustData.fsx
+++ Dust/AnalyzeDustData.fsx
@@ -1,5 +1,18 @@
 ﻿//
-// TODO: get more series (not only 2016)
+// SCOPO:
+// Quanto senso hanno le 'domeniche ecologiche'? Visto che la domenica e' gia' di
+// per se un giorno con particolato basso.
+//
+// SENSORI:
+// --------
+// PM10 = 5
+// PM2.5 = 111
+//
+// STAZIONI:
+// ---------
+// CITTADELLA = 2000003
+// MONTEBELLO = 2000004
+// PARADIGNA = 2000232
 //
 
 #I"../packages/FSharp.Data.2.3.2/lib/net40"
@@ -13,25 +26,34 @@ open System
 open System.IO
 open FSharp.Data
 open FSharp.Charting
+open Deedle
 
-type ARPACsvType = CsvProvider<"data/storico_2016_02000232_111.csv">
+let ccomb = Chart.Combine
 
-let series =
+type ARPACsvType = CsvProvider<"data/storico_2010_02000003_111.csv">
+
+let pm25files = Directory.GetFiles(Path.Combine(__SOURCE_DIRECTORY__,"data"),"*_02000003_111.csv")
+let pm10files = Directory.GetFiles(Path.Combine(__SOURCE_DIRECTORY__,"data"),"*_02000003_005.csv")
+
+let readFiles (flist : seq<string>) =
     seq {
-        for entry in ARPACsvType.GetSample().Rows do
-            yield DateTime.ParseExact(entry.DATA_FINE,"dd\/MM\/yyyy 00",null), entry.VALORE |> float
+        for afile in flist do
+            printfn "-> processing %s" afile
+            for entry in ARPACsvType.Load(afile).Rows do
+                yield DateTime.ParseExact(entry.DATA_INIZIO,"dd\/MM\/yyyy 00",null), entry.VALORE |> float
     } |> List.ofSeq
 
-//Chart.Line(series)
+let pm10series = readFiles pm10files |> Series.ofObservations
+let mavg = pm10series |> Stats.movingMean 90
+let frm = Frame(["pm10";"mavg"],[pm10series;mavg])
+let anomalie = (frm?pm10 - frm?mavg) |> Series.observations
 
-let grouped =
-    query {for entry in series do
-           groupBy (fst entry).DayOfWeek into g
-           let avg =
-            query {
-                    for subentry in g do
-                        averageBy (snd subentry)
-                  }
-           select (g.Key.ToString(),avg)} |> List.ofSeq
+Chart.Rows
+    [
+    Chart.FastLine(mavg |> Series.observations)
+    Chart.FastPoint(anomalie)
+    ]
 
-Chart.Column(grouped)
+let byyday = [for dt,vl in pm10series |> Series.observations -> (float dt.DayOfYear)/366.0, vl]
+Chart.FastPoint(byyday)
+
